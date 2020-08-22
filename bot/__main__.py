2e726186992e9e6c76e6b58564cf7821bc08bec3
Shrimadhav U K <shrimadhavuk@gmail.com>
2020-08-22 20:55:11 +0530

    start message

---
+++ bot/__main__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) The Author(s)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import os
+from pyrogram import (
+    Client
+)
+from bot import (
+    API_HASH,
+    APP_ID,
+    DOWNLOAD_LOCATION,
+    TG_BOT_TOKEN
+)
+
+
+if __name__ == "__main__":
+    if not os.path.isdir(DOWNLOAD_LOCATION):
+        os.makedirs(DOWNLOAD_LOCATION)
+    app = Client(
+        ":memory:",
+        api_hash=API_HASH,
+        api_id=APP_ID,
+        bot_token=TG_BOT_TOKEN,
+        plugins=dict(
+            root="bot/plugins"
+        ),
+        workers=343,
+        workdir=DOWNLOAD_LOCATION
+    )
+    app.set_parse_mode("html")
+    app.run()
---
+++ bot/plugins/start_text.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) The Author(s)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from pyrogram import (
+    Client,
+    Filters,
+    Message
+)
+from bot import (
+    AUTH_USERS,
+    ONLINE_CHECK_START_TEXT,
+    START_OTHER_USERS_TEXT
+)
+
+
+@Client.on_message(
+    Filters.command("start") &
+    ~Filters.chat(AUTH_USERS)
+)
+async def num_start_message(client: Client, message: Message):
+    await message.reply_text(
+        START_OTHER_USERS_TEXT,
+        quote=True
+    )
+
+
+@Client.on_message(
+    Filters.command("start") &
+    Filters.chat(AUTH_USERS)
+)
+async def nimda_start_message(client: Client, message: Message):
+    await message.reply_text(
+        ONLINE_CHECK_START_TEXT,
+        quote=True
+    )
