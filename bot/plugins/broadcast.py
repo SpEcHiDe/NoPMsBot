3e0c5b775d45cc636132d656bfeda31b1f1430a5
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-07-07 20:18:28 +0530

    Update broadcast.py

--- bot/plugins/broadcast.py
+++ bot/plugins/broadcast.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# (c) New-dev0 2021
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
